5641b427c9e457f1db8c7d51295cd6c4e8d70d43
Salvador E. Tropea <stropea@inti.gob.ar>
2025-07-26 11:42:10 -0300

    [Added] Node version during registration

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@
 # Copyright (c) 2025 Instituto Nacional de Tecnologïa Industrial
 # License: GPL-3.0
 # Project: ComfyUI-AudioBatch
-from .src.nodes import nodes_audio, main_logger
+from .src.nodes import nodes_audio, main_logger, __version__
 from seconohe.register_nodes import register_nodes
 from seconohe import JS_PATH
 
 
-NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes(main_logger, [nodes_audio])
+NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes(main_logger, [nodes_audio], version=__version__)
 WEB_DIRECTORY = JS_PATH
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
